0fa76d626d91266ebca29b3c247f3b46b875c12e
Viliam Krizan <vkrizan@redhat.com>
2025-07-08 16:06:49 +0200

    WIP: Vuln Scalprum component with CveListPage

--- webpack/common/ScalprumModule/ScalprumContext.js
+++ webpack/common/ScalprumModule/ScalprumContext.js
@@ -1,45 +1,51 @@
-import React, { useState, createContext, useCallback } from 'react';
+import React, { useState, createContext, useCallback, useMemo } from 'react';
 import PropTypes from 'prop-types';
+import { initialize } from '@scalprum/core'
 import { ScalprumProvider } from '@scalprum/react-core';
 
-export const ScalprumContext = createContext(null);
+//const scStore = initialize()
 
-/* 
-Wrap your component with ScalprumContextWrapper:
- <ScalprumContextWrapper>
-      <WrappedComponent />
-    </ScalprumContextWrapper>
-inside the WrappedComponent (not the component that loads the wrapper), use useEffect and setConfig:
-  const { config, setConfig } = React.useContext(ScalprumContext);
-  const path = 'apps/landing'
-  const scope = 'landing'
-  const module = './AnsibleWidget'
-  const newConfig = {
-    [scope]: {
-      name: scope,
-      manifestLocation: `${window.location.origin}/scalprum/${cdnPath}/fed-mods.json`,
-      cdnPath: `${window.location.origin}/scalprum/${cdnPath}/`,
-    },
-  };
-  useEffect(() => {
-    setConfig(newConfig) 
-  },[])
-
-  Load ScalprumComponent conditionally to the config being set
-  {config[scope] && (
-          <ScalprumComponent scope={scope} module={module}` />
-        )}
-*/
+// export const ScalprumContext = createContext(null);
 
 export const ScalprumContextWrapper = ({ children }) => {
-  const [config, setConfig] = useState({});
-  const contextData = {
-    config,
-    setConfig: useCallback(
-      newConfig => setConfig(prev => ({ ...prev, ...newConfig })),
-      []
-    ),
+  const config = {
+    'vulnerability': {
+      name: 'vulnerability',
+      manifestLocation: `${window.location.origin}/pub/assets/apps/vulnerability/fed-mods.json`,
+      cdnPath: `${window.location.origin}/pub/assets/apps/vulnerability/`,
+    },
   };
+
+  const scStore = useMemo(() => initialize(        {pluginSDKOptions: {
+          pluginLoaderOptions: {
+            transformPluginManifest: manifest => {
+              if (
+                manifest.baseURL === 'auto' &&
+                config[manifest.name]?.cdnPath
+              ) {
+                const _cdnPath = config[manifest.name]?.cdnPath;
+                return {
+                  ...manifest,
+                  baseURL: _cdnPath,
+                  loadScripts: manifest.loadScripts.map(
+                    script => `${_cdnPath}${script}`
+                  ),
+                };
+              }
+              return manifest;
+            },
+          },
+        },
+        api: {
+          chrome: {
+            isBeta: () => false,
+            on: () => {},
+            auth: {
+              getUser: () => Promise.resolve(mockUser),
+            },
+          },
+        },
+        config}), [])
 
   const mockUser = {
     entitlements: {},
@@ -63,50 +69,12 @@ export const ScalprumContextWrapper = ({ children }) => {
       },
     },
   };
-  if (Object.keys(config).length)
-    return (
-      <ScalprumContext.Provider value={contextData}>
-        <ScalprumProvider
-          pluginSDKOptions={{
-            pluginLoaderOptions: {
-              transformPluginManifest: manifest => {
-                if (
-                  manifest.baseURL === 'auto' &&
-                  config[manifest.name]?.cdnPath
-                ) {
-                  const _cdnPath = config[manifest.name]?.cdnPath;
-                  return {
-                    ...manifest,
-                    baseURL: _cdnPath,
-                    loadScripts: manifest.loadScripts.map(
-                      script => `${_cdnPath}${script}`
-                    ),
-                  };
-                }
-                return manifest;
-              },
-            },
-          }}
-          api={{
-            chrome: {
-              isBeta: () => false,
-              on: () => {},
-              auth: {
-                getUser: () => Promise.resolve(mockUser),
-              },
-            },
-          }}
-          config={config}
-        >
-          {children}
-        </ScalprumProvider>
-      </ScalprumContext.Provider>
-    );
-
   return (
-    <ScalprumContext.Provider value={contextData}>
+      <ScalprumProvider
+        scalprum={scStore}
+    >
       {children}
-    </ScalprumContext.Provider>
+    </ScalprumProvider>
   );
 };
 
